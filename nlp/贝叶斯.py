7d320724786291680b8a7d1d85acfe81efca6cf9
chuanfanyoudong <qaz3762541@163.com>
2018-02-27 17:02:59 +0800

    try add commit -

--- nlp/贝叶斯.py
+++ nlp/贝叶斯.py
@@ -1,4 +1,15 @@
+import inspect
+
 import sklearn
 
 # https://www.cnblogs.com/pinard/p/6074222.html https://www.cnblogs.com/Scorpio989/p/4760281.html
 
+import jieba
+
+a = jieba.cut('你好吗')
+#a.__next__()
+for i in a:
+    print(i)
+#print(a.__next__())
+print(jieba.cut.__defaults__())
+jieba.cut.__doc__
